8f1e10170be834f5ce7e6a6cfa030ca4b5c7be50
thevinh106 <lythevinh106@gmail.com>
2024-05-18 21:28:21 +0700

    commit make conflict promblem of lythevinh106

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -20,6 +20,7 @@ class Admin::CategoriesController < AdminController
     end
     puts " day la thay doi commit của vinhIT "
     puts "day la nhanh commit code cuar branch cuar admin the vinh"
+    puts "day la tesst tao ra conflict cua ly the vinh"
     puts "test git hub commit 1"
     @pagy, @categories = pagy(@categories, items: 2)
   end
